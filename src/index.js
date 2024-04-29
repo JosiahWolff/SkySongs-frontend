0eacb719bc52316c8ba878fca656b483e2f82076
Josiah Wolff <josiahjaw0@gmail.com>
2024-04-28 22:51:23 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min.js";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
